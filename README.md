c18d5acda3b3cf36ee14919d83f2a2adb6592579
yizheng <williamzy11@gmail.com>
2019-02-24 12:16:40 -0500

    0224 assignment1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spark Course
+This is from udemy's taming big data with apache spark 
+and python course
+
+all .py file has been slightly modified using some 
+spark2 code.
+
+some files are demo, some are homework. 
---
+++ customer-orders.py
@@ -0,0 +1,19 @@
+# author: yizheng
+from pyspark.sql import SparkSession
+
+
+def main():
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
+    sc = spark.sparkContext
+    orderrdd = sc.textFile("file:///home/williamzy11/codebase/SparkCourse/customer-orders.csv")
+    cust_order = (orderrdd.
+                  map(lambda line: line.split(',')).
+                  map(lambda lt: (lt[0], float(lt[2]))).
+                  reduceByKey(lambda x, y: x+y).
+                  sortBy(lambda tup: tup[1], False))
+    result = cust_order.collect()
+    [print(i) for i in result]
+
+
+if __name__ == '__main__':
+    main()
