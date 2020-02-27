67caeaae578a1da0313c864bce0cc3b50865bfc4
233hehe <williamzy11@gmail.com>
2020-02-26 21:56:16 -0500

    0226

---
+++ leetcode/53_maximum_subarray.py
@@ -0,0 +1,26 @@
+"""
+Given an integer array nums, find the contiguous subarray (containing at least one number)
+ which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+"""
+
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        max_len: int = len(nums)
+        max_res: int = nums[0]
+        max_now: int = 0
+        i: int = 0
+        while i < max_len:
+            max_now += nums[i]
+            if max_now >= max_res:
+                max_res = max_now
+            if max_now < 0:
+                max_now = 0
+            i += 1
+        return max_res
---
+++ leetcode/58_length_of_last_word.py
@@ -0,0 +1,24 @@
+"""
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+return the length of last word
+(last word means the last appearing word if we loop from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+"""
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        split_list = s.split(' ')
+        i = len(split_list) - 1
+        all_flag = True
+        while i >= 0:
+            if split_list[i] == "":
+                i = i - 1
+            else:
+                all_flag=False
+                return len(split_list[i])
+                break
+        if all_flag:
+            return 0
